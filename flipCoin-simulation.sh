882ebe1c70317aeb08db6e62cad7a29593a9e132
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-15 01:09:25 +0530

    show who is winner between tail and head

--- flipCoin-simulation.sh
+++ flipCoin-simulation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
-echo "flip coin simulation problem"
+echo "Flip coin simulation problem" 
+
+#INITIALIZE THE RANDOM VALUE
+random=$(( RANDOM%2 ))
+
+#CHECK THE CONDITION AND DISPLAY HEAD OR TAIL
+if [ $random -eq 1 ]
+then
+	echo "Winner is head"
+else
+	echo "Winner is tail"
+fi
